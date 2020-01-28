0d6a1887a045afb164f0b861884f535493552a12
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-01-28 15:49:36 -0800

    added js and tryna add the random letter generator to go to letters

---
+++ Boggle/boggle.js
@@ -0,0 +1,20 @@
+//The Random Letter Generator
+
+function rando(length){
+    let tile = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let charactersLength = characters.length;
+    for (let i = 0 ; i < length; i++){
+        tile = tile + characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return tile;
+}
+let tiles = rando(16)
+    for (let i = 0; i < tiles.length; i++){
+        let row = Math.floor(i % 4);
+        let column = Math.floor(i % 4);
+        let square = document.getQuerySelector(".board" > div:nth child(" + row + ") >
+        div:nth child(" + column + " )");
+        square.innerHTML = tiles[i];
+    }
+// console.log(rando(1));
