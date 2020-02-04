e9ed5c7563bdbd47338c1928162c4a5b24c6008a
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-02-03 22:34:43 -0800

    added more code

--- Eventonica/eventonicaPart1Classes.js
+++ Eventonica/eventonicaPart1Classes.js
@@ -1,12 +1,20 @@
+
 class User {
     constructor(username){
         this.username = username;
+        this.savedEvents = [];
+    }
+
+    saveEvent(event) {
+        this.savedEvents.push(event);
     }
 }
 
 class Event {
-    constructor(name){
+    constructor(name, date, category){
         this.name = name;
+        this.date = date;
+        this.category = category;
     }
 }
 
@@ -19,33 +27,59 @@ class EventRecommender {
     }
 
     addEvent(event) {
-    // Adds a new Event to the System
+        // Adds a new Event to the System
         this.events.push(event)
     }
 
     addUser(user) {
-    // Adds a new User to the System
+        // Adds a new User to the System
         this.users.push(user)
     }
 
-    saveUserEvent(){
+    saveUserEvent(user, event){
         // Allow users to save events to a personal Events array.
+        user.saveEvent(event)
     }
 
     deleteUser(user) {
-    // Deletes a User from the system
-        for()
+        // Deletes a User from the system
+        this.users = this.users.filter(function(item) { 
+            return item !== user
+        })
     }
    
-    deleteEvent() {
-    // Deletes the Event from the system
+    deleteEvent(event) {
+        // Deletes the Event from the system
+        this.events = this.events.filter(function(item) {
+            return item !== event
+        })
     }
 
-    findEventsByDate(){
-    // Returns all events on a given date
+    findEventsByDate(date){
+        // Returns all events on a given date 
+        return this.events.filter(function(event){
+            return event.date === date;
+        })
     }
     
-    findEventsbyCategory(){
-    // Returns all events in a given category
+    findEventsbyCategory(category){
+        // Returns all events in a given category
+        return this.events.filter(function(event){
+            return event.category === category;
+        })
     }
 }
+
+
+let recommender = new EventRecommender();
+let gabby = new User("gibbyyyy");
+let mitchell = new User("michoooo");
+let kittyPetting = new Event("Pet lots of kitties", "February 5", "animals");
+
+recommender.addUser(mitchell);
+recommender.addUser(gabby);
+recommender.addEvent(kittyPetting);
+recommender.saveUserEvent(gabby, kittyPetting);
+recommender.saveUserEvent(mitchell, kittyPetting);
+
+console.log(recommender.users)
