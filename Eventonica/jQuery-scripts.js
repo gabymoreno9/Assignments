e1775118522271ed6ba283aef9d2e016527beb63
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-02-05 19:57:09 -0800

    adding a user in jQuery

--- Eventonica/jQuery-scripts.js
+++ Eventonica/jQuery-scripts.js
@@ -13,6 +13,15 @@ $(document).ready( () => {
         let user = eventRecommender.users[i];
         $('#all-users').append('<li>' + user.username + '</li>');
     }
-
-    $()
+    //Adding a user
+    $("#add-user input[type='submit']").click(function(e) {
+        //this is to prevent the page from reloading
+        e.preventDefault();
+        // so when we click we want it to push the new user onto
+        // the users array
+        let username = $("#add-user input[type='text']").val();
+        $("#add-user input[type='text']").val('');
+        eventRecommender.addUser(username)
+        $('#all-users').append('<li>' + username + '</li>');
+    })
 });
