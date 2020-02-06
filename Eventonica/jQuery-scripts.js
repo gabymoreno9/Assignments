cb121818d5e2de7e9c37501f3fcbb618d6773c84
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-02-05 20:44:10 -0800

    adding css

--- Eventonica/jQuery-scripts.js
+++ Eventonica/jQuery-scripts.js
@@ -13,7 +13,7 @@ $(document).ready( () => {
         let user = eventRecommender.users[i];
         $('#all-users').append('<li>' + user.username + '</li>');
     }
-    //Adding a user
+    //Show and Adding a user
     $("#add-user input[type='submit']").click(function(e) {
         //this is to prevent the page from reloading
         e.preventDefault();
@@ -24,4 +24,57 @@ $(document).ready( () => {
         eventRecommender.addUser(username)
         $('#all-users').append('<li>' + username + '</li>');
     })
+
+    //Deleting a user
+    $("#delete-user input[type='submit']").click(function(e) {
+        //this is to prevent the page from reloading
+        e.preventDefault();
+        // so when we click we want it to push the new user onto
+        // the users array
+        let username = $("#delete-user input[type='text']").val();
+        $("#delete-user input[type='text']").val('');
+        eventRecommender.deleteUser(username)
+        $('#all-users li').each(function(){
+            if ($(this).html() === username) {
+                $(this).remove();
+            }
+        });
+    })
+
+    //Show, Add Events
+    $("#add-event input[type='submit']").click(function(e) {
+        //this is to prevent the page from reloading
+        e.preventDefault();
+        // so when we click we want it to push the new user onto
+        // the users array
+        let eventName = $("#add-event input[type='text']").val();
+        $("#add-event input[type='text']").val('');
+        eventRecommender.addUser(eventName)
+        $('#all-events').append('<li>' + eventName + '</li>');
+    })
+
+     //Deleting an event
+     $("#delete-event input[type='submit']").click(function(e) {
+        //this is to prevent the page from reloading
+        e.preventDefault();
+        // so when we click we want it to push the new user onto
+        // the users array
+        let eventName = $("#delete-event input[type='text']").val();
+        $("#delete-event input[type='text']").val('');
+        eventRecommender.deleteUser(eventName)
+
+
+        if ($('#all-events li:contains("' + eventName + '")').length === 0) {
+            alert("huh");
+        }
+
+        $('#all-events li').each(function(){
+            if ($(this).html() === eventName) {
+                $(this).remove();
+            }
+        });
+    })
+
+
+
 });
